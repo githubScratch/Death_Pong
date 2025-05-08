26e07527b630bf9e474f18054120315234b63cfb
Blacksoup <mushiii@gmail.com>
2025-05-08 17:06:32 -0400

    sound, display, and reset settings working
    tower changes as well

--- MENUS/settings_menu.gd
+++ MENUS/settings_menu.gd
@@ -3,6 +3,10 @@ extends Control
 @onready var select: AudioStreamPlayer2D = $select
 @onready var move: AudioStreamPlayer2D = $move
 @onready var back_button: Button = $CenterContainer/HBoxContainer/BackCont/BackButton
+@onready var windowed_button: Button = $CenterContainer/HBoxContainer/VBoxContainer/ScreenCont/HBoxContainer/WindowedButton
+@onready var full_button: Button = $CenterContainer/HBoxContainer/VBoxContainer/ScreenCont/HBoxContainer/FullButton
+@onready var bgm_slide: HSlider = $CenterContainer/HBoxContainer/VBoxContainer/SoundCont/HBoxContainer2/BGMSlide
+@onready var sfx_slide: HSlider = $CenterContainer/HBoxContainer/VBoxContainer/SoundCont/HBoxContainer3/SFXSlide
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -22,3 +26,23 @@ func _process(delta: float) -> void:
 
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://MENUS/Mode_Menu.tscn")
+
+
+func _on_windowed_button_pressed() -> void:
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	full_button.set_pressed_no_signal(false)
+	windowed_button.set_pressed_no_signal(true)
+	
+func _on_full_button_pressed() -> void:
+	#DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+	full_button.set_pressed_no_signal(true)
+	windowed_button.set_pressed_no_signal(false)
+
+
+func _on_reset_button_pressed() -> void:
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+	full_button.set_pressed_no_signal(true)
+	windowed_button.set_pressed_no_signal(false)
+	bgm_slide.set_value(1)
+	sfx_slide.set_value(1)
